27db0a3a937f17f434e1166ed495654c7e28d9de
Millie <jay.lee.2280@gmail.com>
2022-11-11 12:11:52 +0900

    [#6] Feat: redirect user to login page when token is not valid

--- client/src/layout/index.tsx
+++ client/src/layout/index.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { NavBar } from './NavBar';
 
+import { USER_TOKEN } from '@/constants/constants';
+import useMovePage from '@/hooks/useMovePage';
+
 export const Layout = () => {
+  const [goLogin] = useMovePage('/login');
+
+  useEffect(() => {
+    const userToken = localStorage.getItem(USER_TOKEN);
+    if (!userToken) {
+      alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');
+      goLogin();
+    }
+  }, []);
+
   return (
     <>
       <NavBar />
