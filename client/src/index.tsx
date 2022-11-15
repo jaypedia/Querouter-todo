2575a19f03a135e5fb9d9540bf69246b69b4b032
Millie <jay.lee.2280@gmail.com>
2022-11-15 17:10:44 +0900

    [#10] Refactor: apply Strict mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { API_END_POINT } from './constants/constants';
@@ -8,4 +9,8 @@ import { App } from '@/App';
 const root = createRoot(document.getElementById('root') as HTMLDivElement);
 axios.defaults.baseURL = API_END_POINT;
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
