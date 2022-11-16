507ffea5ce04e7cc9b57f265b42423a2f2b620ac
Millie <jay.lee.2280@gmail.com>
2022-11-16 23:31:32 +0900

    [#10] Refactor: remove React.FC type

--- client/src/components/common/TextArea/index.tsx
+++ client/src/components/common/TextArea/index.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
+import { forwardRef, TextareaHTMLAttributes } from 'react';
 
 import * as S from './style';
 
-type TextAreaProps = {
-  name: string;
-  defaultValue?: string;
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   onChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
-  value?: string;
-};
+}
 
-type I = React.ComponentPropsWithRef<'textarea'> & TextAreaProps;
-
-const TextArea: React.FC<I> = React.forwardRef(({ name, defaultValue, onChange, value }, ref) => {
-  return (
-    <S.TextAreaWrapper>
-      <S.TextArea
-        id="description"
-        name={name}
-        placeholder="Today I should do..."
-        defaultValue={defaultValue}
-        ref={ref}
-        onChange={onChange}
-        value={value}
-      />
-    </S.TextAreaWrapper>
-  );
-});
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ name, defaultValue, onChange, value }, ref) => {
+    return (
+      <S.TextAreaWrapper>
+        <S.TextArea
+          id="description"
+          name={name}
+          placeholder="Today I should do..."
+          defaultValue={defaultValue}
+          ref={ref}
+          onChange={onChange}
+          value={value}
+        />
+      </S.TextAreaWrapper>
+    );
+  },
+);
 
 export default TextArea;
