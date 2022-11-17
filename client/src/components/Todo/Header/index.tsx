b726b97aabe3d021ef1800430be8a05954ff99d2
Millie <jay.lee.2280@gmail.com>
2022-11-17 18:18:49 +0900

    Refactor: use `toLocaleDateString` to show date

--- client/src/components/Todo/Header/index.tsx
+++ client/src/components/Todo/Header/index.tsx
@@ -2,13 +2,15 @@ import * as S from './style';
 
 import { Button } from '@/components/common/Button';
 import useMovePage from '@/hooks/useMovePage';
+import { getDate } from '@/utils/date';
 
 export const Header = () => {
   const [goNewTodo] = useMovePage('/new-todo');
+  const date = getDate();
 
   return (
     <S.HeaderWrapper>
-      Nov 13 Sunday
+      {date}
       <Button size="xSmall" background="grey" text="New Todo" onClick={goNewTodo} />
     </S.HeaderWrapper>
   );
