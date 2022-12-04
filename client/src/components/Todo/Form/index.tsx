0c75a7ee131f10a2401ace5236cfc0fdb296cf24
Millie <jay.lee.2280@gmail.com>
2022-12-04 22:14:19 +0900

    [#22] Refactor: modify Form component

--- client/src/components/Todo/Form/index.tsx
+++ client/src/components/Todo/Form/index.tsx
@@ -1,57 +1,31 @@
-import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import * as S from './style';
 
-import { updateTodo } from '@/apis/todoApi';
 import { Button } from '@/components/common/Button';
 import { Input } from '@/components/common/Input';
 import TextArea from '@/components/common/TextArea';
-import { useRefetchTodo, useRefetchTodoDetail } from '@/queries/todo';
 import { FlexEnd } from '@/styles/common';
 import { Todo } from '@/types/todoType';
 
-type FormProps = {
-  onCancel: () => void;
-  editTodoData: Pick<Todo, 'id' | 'title' | 'content'>;
-};
-
-export const Form = ({ onCancel, editTodoData }: FormProps) => {
-  const titleRef = useRef<HTMLInputElement>(null);
-  const contentRef = useRef<HTMLTextAreaElement>(null);
-  const { mutate } = useRefetchTodo();
-  const todoId = editTodoData.id;
-  const { mutate: mutateDetail } = useRefetchTodoDetail(todoId);
+export const Form = ({ editTodoData }: { editTodoData: Todo }) => {
+  const navigate = useNavigate();
 
   const handleCancelClick = () => {
-    onCancel();
-  };
-
-  const handleSubmit = async () => {
-    if (!titleRef.current || !contentRef.current) return;
-
-    const todoData = {
-      title: titleRef.current.value,
-      content: contentRef.current.value,
-    };
-
-    await updateTodo({ id: todoId, ...todoData });
-    mutate();
-    mutateDetail();
-    onCancel();
+    navigate(-1);
   };
 
   return (
-    <S.TodoForm>
+    <S.TodoForm method="post">
       <Input
         name="title"
         placeholder="Title"
         inputStyle="medium"
         title="Title"
         type="text"
-        ref={titleRef}
         defaultValue={editTodoData.title}
       />
-      <TextArea name="content" ref={contentRef} defaultValue={editTodoData.content} />
+      <TextArea name="content" defaultValue={editTodoData.content} />
       <FlexEnd>
         <Button
           size="small"
@@ -60,13 +34,7 @@ export const Form = ({ onCancel, editTodoData }: FormProps) => {
           type="button"
           onClick={handleCancelClick}
         />
-        <Button
-          size="small"
-          background="primary"
-          text="Update todo"
-          type="button"
-          onClick={handleSubmit}
-        />
+        <Button size="small" background="primary" text="Update todo" type="submit" />
       </FlexEnd>
     </S.TodoForm>
   );
--- client/src/components/Todo/Form/style.ts
+++ client/src/components/Todo/Form/style.ts
@@ -1,8 +1,9 @@
+import { Form } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { flexBox } from '@/styles/flexBox';
 
-const TodoForm = styled.form`
+const TodoForm = styled(Form)`
   ${flexBox({ direction: 'column' })}
   gap: 10px;
   width: 100%;
