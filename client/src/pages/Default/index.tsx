d9357af92d3b2f14083f48fbe730b15604fcb197
Jayun <jay.lee.2280@gmail.com>
2023-05-09 00:03:10 +0900

    Feat: implement Default page

---
+++ client/src/pages/Default/index.tsx
@@ -0,0 +1,10 @@
+import { DetailContainer, Heading2 } from '@/styles/common';
+
+export const Default = () => {
+  return (
+    <DetailContainer>
+      <Heading2>Hello, This is Star Todo!</Heading2>
+      Make your day shine with Star Todo ✨
+    </DetailContainer>
+  );
+};
